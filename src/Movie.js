cfe7270073e8cde8ec1a9d37c642399a0aea5ddb
Oleh Kozlovskyi <kozlovskuioleg@gmail.com>
2018-03-16 17:32:11 +0200

    add PropTypes

--- src/Movie.js
+++ src/Movie.js
@@ -3,13 +3,21 @@ import PropTypes from 'prop-types';
 
 class Movie extends Component {
   static propTypes = {
+    movie: PropTypes.arrayOf(PropTypes.shape({
+      title: PropTypes.string.isRequired,
+    })),
+    desc: PropTypes.string.isRequired
+  }
 
+  static defaultProps = {
+    desc: 'Desc not available'
   }
 
   render() {
     return (
       <div>
-        {this.props.movie.title}
+        <h3>{this.props.movie.title}</h3>
+        <p>{this.props.desc}</p>
       </div>
     );
   }
